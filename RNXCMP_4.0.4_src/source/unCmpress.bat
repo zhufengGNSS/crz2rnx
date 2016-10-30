f4f729d806976958921dbe2bc5cc34691fbbf2de
zlongshen <zlongshen@outlook.com>
2016-10-30 16:21:13 +0800

    this is the CRZ2RNX project, we create an vs solution to build this exe on windows. to get the compressed file from IGS site, and uncompress the observation files to the standard rinex format.

---
+++ RNXCMP_4.0.4_src/source/unCmpress.bat
@@ -0,0 +1,4 @@
+path=%path%;F:\BaiduYunDownload\rinex-compress\RNXCMP_4.0.4\RNXCMP_4.0.4_src\RNXCMP_4.0.4_src\source\;
+rem call "F:\BaiduYunDownload\rinex-compress\RNXCMP_4.0.4\RNXCMP_4.0.4_src\RNXCMP_4.0.4_src\source\crx2rnx_run.bat"  *.??d
+call  crx2rnx_run.bat   *.??d
+pause
